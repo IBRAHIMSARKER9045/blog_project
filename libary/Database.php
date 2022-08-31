df9119f57bae125fbd5c451f9f82c41f29237848
ibrahim597 <ibrahim.sarkar597@gmail.com>
2022-08-31 21:34:07 +0600

    blog previous work upload

---
+++ libary/Database.php
@@ -0,0 +1,68 @@
+<?php
+ class Database{
+    public $host = HOST;
+    public $user = USER;
+    public $password = PASSWORD;
+    public $database = DATABASE; 
+
+    public $link;
+    public $error; 
+
+    public function __construct()
+    {
+      $this->dbConnect();
+    }
+
+    public function dbConnect(){
+    $this->link =mysqli_connect($this->host,$this->user,$this->password,$this->database);
+    if (!$this->link){
+      $this->error = "Database connection fail";
+      return false;
+
+    }
+
+    }
+
+  //select Quary
+  public function select($query){
+   $result = mysqli_query($this->link, $query)or die($this->error__line__);
+   if(mysqli_num_rows($result)>0){
+      return $result;
+   }
+   else{
+      return false;
+   }
+  }
+  //insert Quary
+  public function insert($query){
+   $result = mysqli_query($this->link, $query)or die($this->error__line__);
+   if($result){
+      return $result;
+   }
+   else{
+      return false;
+   }
+  }
+  //Update Quary
+  public function update($query){
+   $result = mysqli_query($this->link, $query)or die($this->error__line__);
+   if($result){
+      return $result;
+   }
+   else{
+      return false;
+   }
+  }
+  //Delete Quary
+  public function delete($query){
+   $result = mysqli_query($this->link, $query)or die($this->error__line__);
+   if($result){
+      return $result;
+   }
+   else{
+      return false;
+   }
+  }
+ }
+
+?>         
