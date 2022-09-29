0094704ba3ac9edd18d2cd2962cba0ac9c2bcf75
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-09-29 10:02:24 +0600

    varify token

--- classes/register.php
+++ classes/register.php
@@ -3,6 +3,15 @@
 include_once '../libary/Database.php';
 include_once '../helpers/formet.php';
 
+include_once '../PHPMailer/PHPMailer.php';
+include_once '../PHPMailer/SMTP.php';
+include_once '../PHPMailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
 class register{
     public $db;
     public $fr;
@@ -14,7 +23,33 @@ class register{
 
     public function addUser($data){
 
-        function ssendemail_varifi($name,$email,$v_token){}
+        function ssendemail_varifi($name,$email,$v_token){
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Username = 'ibrahim.sarkar597@gmail.com';
+        $mail->Password = 'denapaona00';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+        $mail->setFrom('ibrahim.sarkar597@gamil.com', $name);
+        $mail->addAddress($email);
+        $mail->isHTML(true);
+        $mail->Subject = 'Email Varification';
+        
+        
+        $email_tamplate ="
+        <h2>you have to varify your email</h2>
+        <h5>click the link below to varify your email</h5>
+        <a href='http://localhost/R_49/PHP_ALL_PROJECT/blog_project/admin/verify-email.php?
+        token=$v_token'>Varify Email</a>";
+        $mail->Body = $email_tamplate;
+        $mail->send();
+
+
+
+
+        }
 
         $name = $this->fr->validation($data['name']);
         $email = $this->fr->validation($data['email']);
