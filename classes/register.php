d056cc306be603dece2e223ae6dd0ada61f90e50
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-09-29 09:37:49 +0600

    php mailer

--- classes/register.php
+++ classes/register.php
@@ -13,25 +13,45 @@ class register{
     }
 
     public function addUser($data){
+
+        function ssendemail_varifi($name,$email,$v_token){}
+
         $name = $this->fr->validation($data['name']);
-        $phone = $this->fr->validation($data['phone']);
         $email = $this->fr->validation($data['email']);
+        $phone = $this->fr->validation($data['phone']);
         $password = $this->fr->validation($data['password']);
         $v_token= md5(rand());
 
+             
+      
 
-       $e_query = "SELECT * FROM users WHERE email ='$email'";
-        $check_email = $this->db->select($e_query);
-
-        if($check_email > 0){
-            $msg = "Email already exists";
-            return $error;
-            header("Location: register.php");
-        }
-
-            if(empty($name) || empty($phone) || empty($email) || empty($password)){
+            if(empty($name) || empty($email) || empty($phone) || empty($password)){
                 $error = "Field must not be empty";
                 return $error;
+            }
+            else{ $e_query = "SELECT * FROM users WHERE email ='$email'";
+                $check_email = $this->db->select($e_query);
+        
+                if($check_email > 0){
+                    $msg = "Email already exists";
+                    return $error;
+                    header("Location: register.php");
+                }
+                else{
+                    $query = "INSERT INTO users(name,email,phone,password,v_token) VALUES('$name','$email','$phone','$password','$v_token')";
+                    $insert_row = $this->db->insert($query);
+                    if($insert_row){
+                        sendemail_varifi($name,$email,$v_token);
+                        $msg = "Registration Successfull";
+                        return $msg;
+                    }
+                    else{
+                        $error = "Registration Failed";
+                        return $error;
+                    }
+
+                     
+                }   
             }
         }
     }
