43702a03e70a393335205a9239cc46cbb1c4f07c
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-10-08 08:48:08 +0600

    dissmissable alert in Add ctegory page

--- admin/postadd.php
+++ admin/postadd.php
@@ -14,7 +14,26 @@ include_once 'inc/sidebar.php';
 <div class="main-content">
     <div class="page-content">
         <div class="container-fluid">
-            <!--add post form-->$_COOKIE
+            <div class="row">
+                <div class="col-12">
+
+                <span>
+
+                <?php
+                if(isset($catdAdd)){
+                    ?>
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong><?php echo $catdAdd; ?></strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    <?php
+                }
+                ?>
+                
+                    </span>
+        
           
         </div>
     </div>
